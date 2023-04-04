e9de453397654d4a9bda3523541b8307e623d405
dat nguyen <datnguyentanpts@gmail.com>
2023-04-04 23:24:41 +0700

    commit bt1

--- bt1.js
+++ bt1.js
@@ -1,1 +1,13 @@
-console.log('hello')
+function sum () {
+    if (arguments.length <= 0) return
+    let i = 0
+    for (const value of Object.values(arguments)) {
+        i = i + value
+    }
+
+    return i
+}
+
+console.log(sum(3,5,7))
+console.log(sum(10,-6,3))
+console.log(sum())
