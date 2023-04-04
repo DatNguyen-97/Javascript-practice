734ffea940ff62151142367b003ffbb684b011d6
dat nguyen <datnguyentanpts@gmail.com>
2023-04-05 00:43:18 +0700

    commit bt4

---
+++ bt4.js
@@ -0,0 +1,13 @@
+function caculator (n) {
+    if (n <= 0) return
+
+    let result = 0
+
+    for(let i = 1; i <= n; i++) {
+        result = result + 1/(i*2)
+    }
+
+    return result
+}
+
+console.log(caculator(5))
