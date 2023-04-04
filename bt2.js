d91f78de8e72b654f376da589267b52667064801
dat nguyen <datnguyentanpts@gmail.com>
2023-04-05 01:18:13 +0700

    commit bt2

--- bt2.js
+++ bt2.js
@@ -13,10 +13,10 @@ console.log(caculator(1,2,3))
 console.log(caculator())
 
 function caculator2 (n) {
-    if (n <= 0) return
+    if (n < 0) return
     let result = 0
 
-    for(let i = 1; i <= n; i++) {
+    for(let i = 0; i <= n; i++) {
         result = result + Math.pow(i, 2)
     }
 
